0d8f9a73c0aa7b2a0b472057565f8b3387af1338
akhilesh3721 <akhilesh.s3721@gmail.com>
2026-06-21 11:17:45 +0530

    Update memory.py

--- memory.py
+++ memory.py
@@ -1,9 +1,9 @@
 import os
 from supabase import create_client
-print("SUPABASE_URL =", url)
+
 url = os.getenv("SUPABASE_URL")
 key = os.getenv("SUPABASE_KEY")
-
+print("SUPABASE_URL =", url)
 supabase = create_client(url, key)
 
 def save_memory(user_id, username, message):
